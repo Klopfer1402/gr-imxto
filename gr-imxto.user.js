5bcd59070414abcc2a8011dc8c2af2cd97774e66
Klopfer <klopfer@klopfers-web.de>
2022-08-20 19:46:43 +0200

    Changed language to English

--- gr-imxto.user.js
+++ gr-imxto.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @name         IMX.TO Direktlink auf GirlsReleased
+// @name         IMX.TO Direktlink on GirlsReleased
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.21
 // @description  Replaces Image URIs on GirlsReleased with direct links to the image files on imx.to
 // @author       Christian Schmidt
+// @updateURL    https://github.com/Klopfer1402/gr-imxto/raw/main/gr-imxto.user.js
 // @match        https://girlsreleased.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=girlsreleased.com
 // @grant        none
@@ -17,7 +18,7 @@
     const auswahl = document.createElement('div');
     auswahl.style.fontSize = '0.7em';
     auswahl.style.padding = '1em';
-    auswahl.innerHTML = '<form id="imxtoauswahl"><label for="imxtoselect">IMX.TO-Server:</label><select name="imxtoselect" id="imxtoselect" size="1"><option value="i" selected>i</option><option value="i001">i001</option><option value="i002">i002</option><option value="i003">i003</option></select>.imx.to <button id="imxtoselectbtn">Auswahl</button></form>';
+    auswahl.innerHTML = '<form id="imxtoauswahl"><label for="imxtoselect">imx.to image server:</label><select name="imxtoselect" id="imxtoselect" size="1"><option value="i" selected>i</option><option value="i001">i001</option><option value="i002">i002</option><option value="i003">i003</option></select>.imx.to <button id="imxtoselectbtn">Select</button></form>';
     const content = document.querySelector('#set > .content');
     content.prepend(auswahl);
 
