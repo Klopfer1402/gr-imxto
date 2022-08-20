45d4c71cc3f00d8ee5ccc71fec8746e1f97b54dc
Klopfer <klopfer@klopfers-web.de>
2022-08-20 20:15:02 +0200

    Metadata added

--- gr-imxto.user.js
+++ gr-imxto.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         IMX.TO Direktlink on GirlsReleased
+// @name:de      Direkte Bildlinks auf Girlsreleased.com
 // @namespace    http://tampermonkey.net/
 // @version      0.21
 // @description  Replaces Image URIs on GirlsReleased with direct links to the image files on imx.to
+// @description:de Ersetzt die Bild-Links auf GirlsReleased mit direkten Links zu den Bilddateien auf imx.to
 // @author       Christian Schmidt
 // @updateURL    https://github.com/Klopfer1402/gr-imxto/raw/main/gr-imxto.user.js
 // @match        https://girlsreleased.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=girlsreleased.com
 // @grant        none
+// @license		 MIT
 // ==/UserScript==
 
 (function() {
