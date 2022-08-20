646089a9224c04f4f3e20aff25e5e862c37eec01
Klopfer1402 <klopfer666@gmx.de>
2022-08-20 19:23:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Direct Image Links in Girlsreleased.com
+
+Links to images in sets on Girlsreleased.com usually lead to imx.to, an image host which tends to go overboard with ad redirects, often to sites of questionable reputation and attempts to get you to install malware on your PC. This script for Tampermonkey/Greasemonkey/Violentmonkey and similar browser add-ons enables to change the links to the actual image servers imx.to uses.
+
+## Usage
+
+When Girlsreleased presents you with the thumbnails in a set, try the server options and press the button. The background color of the Thumbnails should change. Try all server options until the link leads you to the desired picture.
---
+++ gr-imxto.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         IMX.TO Direktlink auf GirlsReleased
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Replaces Image URIs on GirlsReleased with direct links to the image files on imx.to
+// @author       Christian Schmidt
+// @match        https://girlsreleased.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=girlsreleased.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    const urlpart = '.imx.to/i/';
+    const auswahl = document.createElement('div');
+    auswahl.style.fontSize = '0.7em';
+    auswahl.style.padding = '1em';
+    auswahl.innerHTML = '<form id="imxtoauswahl"><label for="imxtoselect">IMX.TO-Server:</label><select name="imxtoselect" id="imxtoselect" size="1"><option value="i" selected>i</option><option value="i001">i001</option><option value="i002">i002</option><option value="i003">i003</option></select>.imx.to <button id="imxtoselectbtn">Auswahl</button></form>';
+    const content = document.querySelector('#set > .content');
+    content.prepend(auswahl);
+
+    const awbutton = document.getElementById("imxtoselectbtn");
+    if (awbutton != null) {
+        awbutton.addEventListener("click", function(e) {
+            e.preventDefault();
+            const aw = document.getElementById("imxtoselect").value;
+            if (!aw) return;
+            let servername = aw + urlpart;
+            let thumblist = document.querySelectorAll('ul.setthumbs > li');
+            if (thumblist.length == 0) return;
+            [...thumblist].forEach(ele => {
+               const a = ele.querySelector('a');
+               const img = ele.querySelector('img');
+               const thumbimgsrc = img.src;
+               const neubildsrc = thumbimgsrc.replace('https://imx.to/u/t/', 'https://' + servername);
+               a.href = neubildsrc;
+               let bgcolor = 'PowderBlue';
+               switch (aw) {
+                   case 'i001': bgcolor = 'AliceBlue'; break;
+                   case 'i002': bgcolor = 'Azure'; break;
+                   case 'i003': bgcolor = 'PaleTurquoise'; break;
+                   default: bgcolor = 'PowderBlue'; break;
+               }
+               ele.style.backgroundColor = bgcolor;
+            });
+        });
+    }
+})();
