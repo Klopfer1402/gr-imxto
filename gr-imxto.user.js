78582c9b15aa87521539ce7f70abe008c4d619e7
Klopfer <klopfer@klopfers-web.de>
2022-09-13 22:18:43 +0200

    Fix: Imagetwist links with JPG in capital letters fixed.

--- gr-imxto.user.js
+++ gr-imxto.user.js
@@ -2,7 +2,7 @@
 // @name         IMX.TO Direktlink on GirlsReleased
 // @name:de      Direkte Bildlinks auf Girlsreleased.com
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Replaces Image URIs on GirlsReleased with direct links to the image files on imx.to or imagetwist.com
 // @description:de Ersetzt die Bild-Links auf GirlsReleased mit direkten Links zu den Bilddateien auf imx.to oder imagetwist.com
 // @author       Christian Schmidt
@@ -20,9 +20,10 @@
     const auswahl = document.createElement('div');
     const imxtoptn = /imx\.to/;
     const imgtwistptn = /imagetwist\.com/;
+    const imgtwistbigjpg = /\.JPG/;
     auswahl.style.fontSize = '0.7em';
     auswahl.style.padding = '1em';
-    auswahl.innerHTML = '<form id="imxtoauswahl"><label for="imxtoselect">imx.to image server:</label><select name="imxtoselect" id="imxtoselect" size="1"><option value="i" selected>i</option><option value="i001">i001</option><option value="i002">i002</option><option value="i003">i003</option></select>.imx.to <button id="imxtoselectbtn">Select</button></form>';
+    auswahl.innerHTML = '<form id="imxtoauswahl"><label for="imxtoselect">imx.to image server:</label><select name="imxtoselect" id="imxtoselect" size="1"><option value="i" selected>i</option><option value="i001">i001</option><option value="i002">i002</option><option value="i003">i003</option></select>.imx.to</label> <button id="imxtoselectbtn">Select</button></form>';
     const content = document.querySelector('#set > .content');
     content.prepend(auswahl);
 
@@ -53,7 +54,12 @@
                     ele.style.backgroundColor = bgcolor;
                 }
                 if (imgtwistptn.test(thumbimgsrc)) {
-                    const neubildsrc = thumbimgsrc.replace('imagetwist.com/th/', 'imagetwist.com/i/') + '/' + img.alt;
+                    let neubildsrc = thumbimgsrc;
+                    if (imgtwistbigjpg.test(img.alt)) {
+                        neubildsrc = neubildsrc.replace('.jpg', '.JPG');
+                    }
+                    neubildsrc = neubildsrc.replace('imagetwist.com/th/', 'imagetwist.com/i/') + '/' + img.alt;
+
                     a.href = neubildsrc;
                     ele.style.backgroundColor = 'Lavender';
                 }
